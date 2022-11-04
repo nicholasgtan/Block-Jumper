8fb525c3630a57f3dd8caf64afcdd811ddea7863
Nicholas Tan <nicholasgtan@gmail.com>
2022-11-05 01:39:24 +0800

    Created player and placed on game screen

--- main.js
+++ main.js
@@ -1,2 +1,44 @@
 import "./style.css";
 import $ from "jquery";
+
+$(() => {
+  let canvasWidth = 600;
+  let canvasHeight = 400;
+
+  let player = {};
+  let playerYPosition = 200;
+
+  const startGame = () => {
+    gameCanvas.start();
+    player = new createPlayer();
+    console.log(player);
+  };
+  // Data
+  // 1. game screen and render
+  const gameCanvas = {
+    canvas: $("<canvas>"),
+    start: () => {
+      gameCanvas.canvas[0].width = canvasWidth;
+      gameCanvas.canvas[0].height = canvasHeight;
+      gameCanvas.context = gameCanvas.canvas.get(0).getContext("2d");
+      gameCanvas.canvas.insertAfter($("h1"));
+    },
+  };
+
+  // 2. player on game screen
+  function createPlayer() {
+    player.width = 30;
+    player.height = 30;
+    player.x = 10;
+    player.y = playerYPosition;
+    let ctx = gameCanvas.context;
+    ctx.fillStyle = "#663399";
+    ctx.fillRect(player.x, player.y, player.width, player.height);
+  }
+
+  // 3. obstacle blocks with location moving toward player
+  // 4. obstacle reaching end
+  // 5. collision between player and obstacle
+
+  startGame();
+});
